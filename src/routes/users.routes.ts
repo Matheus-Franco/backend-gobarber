faecd780de1505e24ad5f4d322f59b11270bff24
Matheus Franco <matheusfrancoalves@hotmail.com>
2020-06-25 15:59:07 -0300

    User Registration

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import { hash } from 'bcryptjs';
+
+import CreateUserService from '../services/CreateUserService';
+
+const userRouter = Router();
+
+userRouter.post('/', async (request, response) => {
+    try {
+        const { name, email, password } = request.body;
+
+        const createUser = new CreateUserService();
+
+        const hashedPassword = await hash(password, 8);
+
+        const user = await createUser.execute({
+            name,
+            email,
+            password: hashedPassword,
+        });
+
+        delete user.password;
+
+        return response.json(user);
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+export default userRouter;
