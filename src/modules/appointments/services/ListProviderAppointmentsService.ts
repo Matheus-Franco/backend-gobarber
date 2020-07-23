324c09951e78c907a3ac90f23d4483f2b469a3c1
Matheus Franco <matheusfrancoalves@hotmail.com>
2020-07-23 16:10:36 -0300

    Provider's agenda

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+import { inject, injectable } from 'tsyringe';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+    provider_id: string;
+    month: number;
+    year: number;
+    day: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+    constructor(
+        @inject('AppointmentsRepository')
+        private appointmentsRepository: IAppointmentsRepository
+    ) { }
+
+    public async execute({ provider_id, month, year, day }: IRequest): Promise<Appointment[]> {
+        const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
+            provider_id,
+            day,
+            month,
+            year,
+        });
+
+        return appointments;
+    }
+}
+
+export default ListProviderAppointmentsService;
