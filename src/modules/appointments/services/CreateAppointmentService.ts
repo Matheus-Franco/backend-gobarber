7d75bd8c3d564697f77973cbb5805b6e758baa86
Matheus Franco <matheusfrancoalves@hotmail.com>
2020-07-23 22:00:56 -0300

    Send notifications

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,12 +1,15 @@
 import "reflect-metadata"
 
-import { startOfHour, isBefore, getHours } from 'date-fns';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+import Notification from '@modules/notifications/infra/typeorm/schemas/Notification';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
 
 interface IRequest {
     provider_id: string;
@@ -18,7 +21,11 @@ interface IRequest {
 class CreateAppointmentService {
     constructor(
         @inject('AppointmentsRepository')
-        private appointmentsRepository: IAppointmentsRepository) { }
+        private appointmentsRepository: IAppointmentsRepository,
+
+        @inject('NotificationsRepository')
+        private notificationsRepository: INotificationsRepository,
+    ) { }
 
     public async execute({ provider_id, user_id, date }: IRequest): Promise<Appointment> {
         const appointmentDate = startOfHour(date);
@@ -48,6 +55,13 @@ class CreateAppointmentService {
             user_id,
             provider_id,
             date: appointmentDate,
+        });
+
+        const dateFormatted = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+
+        await this.notificationsRepository.create({
+            recipient_id: provider_id,
+            content: `Novo agendamento para o dia ${dateFormatted}`
         });
 
         return appointment;
